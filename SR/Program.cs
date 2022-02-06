7d825ff0b9c9a3f15165f8a5b63ca5f1814bc6c6
reXt88 <seva88best.com@yandex.ru>
2022-02-06 13:15:41 +0300

    Добавьте файлы проекта.

---
+++ SR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR
+{
+    internal class Program
+    {
+        //создание метода вычисления процента
+        static void AddPersent(double a, out double b)
+        {
+            b = 0;
+            if (a < 100) b = a + a / 100 * 5;
+            if (a >= 100 && a < 200) b = a + a / 100 * 7;
+            if (a >= 200) b = a + a / 100 * 10;
+            Console.WriteLine($"Сумма с начисленными процентами = {b:0.0}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму вклада ");
+            double a =Convert.ToDouble(Console.ReadLine());
+            //вызов созданного метода
+            AddPersent(a, out double b);
+            Console.ReadKey();
+        }
+    }
+}
